98b2fcecc479816ede73bbdb106eb22aa757104d
Chosen2730 <70141158+Chosen2730@users.noreply.github.com>
2022-08-08 00:42:04 +0100

    fixed

--- components/nav/index.js
+++ components/nav/index.js
@@ -12,7 +12,7 @@ const Nav = () => {
     <nav className='shadow-md'>
       <div className='mx-auto max-w-6xl flex flex-col md:flex-row justify-between md:items-center py-4 px-4 xl:px-0  text-[#0F6212]'>
         <div className='flex justify-between items-center'>
-          <Image src={logo} width={112} height={58} />
+          <Image src={logo} width={112} height={58} alt='logo' />
           <i
             className='block md:hidden text-3xl'
             onClick={() => setNavOpen(!navOpen)}
