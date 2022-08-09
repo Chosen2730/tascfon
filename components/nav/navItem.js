7cf055f315a5a084c3a506718b8e15c7cb6cb8c6
Chosen2730 <70141158+Chosen2730@users.noreply.github.com>
2022-08-09 16:44:20 +0100

    completed landing apge

--- components/nav/navItem.js
+++ components/nav/navItem.js
@@ -1,8 +1,11 @@
 import { useState } from "react";
 import { IoMdArrowDropright } from "react-icons/io";
 import { MdArrowDropDown } from "react-icons/md";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
-const NavItem = ({ title, sub, id }) => {
+const NavItem = ({ title, url, sub, id }) => {
+  const router = useRouter();
   const [drop, setDrop] = useState(false);
   const [base, setBase] = useState(0);
   return (
@@ -12,9 +15,12 @@ const NavItem = ({ title, sub, id }) => {
       }}
     >
       <div className='flex items-center'>
-        <h1 className='uppercase my-4 font-bold text-sm cursor-pointer select-none py-2 border-white hover:text-[#FF0000] border-b-2 hover:border-[#FF0000] transition'>
+        <h1
+          className='uppercase my-4 font-bold text-sm cursor-pointer select-none py-2 border-white hover:text-[#FF0000] border-b-2 hover:border-[#FF0000] transition'
+          onClick={() => router.push(url)}
+        >
           {title}
-        </h1>
+        </h1>{" "}
         {sub && (
           <i className='text-xl'>
             {!drop ? <MdArrowDropDown /> : <IoMdArrowDropright />}
@@ -25,16 +31,18 @@ const NavItem = ({ title, sub, id }) => {
         <div
           className={`${
             drop ? "block" : "hidden"
-          } md:absolute z-10 top-24 border-t-4 border-[#FF0000] bg-white p-4 rounded-md shadow-md`}
+          } md:absolute z-10 top-24 border-t-4 border-[#FF0000] bg-white p-4 px-8 rounded-md shadow-md`}
         >
-          {sub?.map((item, i) => {
+          {sub?.map(({ title, url }, i) => {
             return (
-              <h1
-                key={i}
-                className='hover:text-[#FF0000] transition uppercase my-4 font-bold text-sm cursor-pointer'
-              >
-                {item}
-              </h1>
+              <Link href={url}>
+                <h1
+                  key={i}
+                  className='hover:text-[#FF0000] transition uppercase my-4 font-bold text-sm cursor-pointer'
+                >
+                  {title}
+                </h1>
+              </Link>
             );
           })}
         </div>
--- utils/navItems.js
+++ utils/navItems.js
@@ -1,26 +1,37 @@
 export const navtitles = [
-  { title: "home" },
-  { title: "about us" },
+  { title: "home", url: "/" },
+  { title: "about us", url: "about" },
   {
     title: "information center",
+    url: "#",
     sub: [
-      "prayer request",
-      "testimony",
-      "executives",
-      "course materials",
-      "upcoming events",
-      "contact us",
+      { title: "executives", url: "/executives" },
+      { title: "contact us", url: "/contact" },
+      { title: "Family Song", url: "/song" },
     ],
   },
   {
     title: "resources",
+    url: "#",
     sub: [
-      "course materials",
-      "past questions",
-      "inspirations books",
-      "sermons/messages",
+      {
+        title: "course materials",
+        url: "https://drive.google.com/drive/folders/1XuzEzopVvmNaLI1r5AlSTrkJhQvGOhFp",
+      },
+      {
+        title: "past questions",
+        url: "https://drive.google.com/drive/folders/1xcJZh9cpU8oDVFVsw_hHYZvL0LV7CX70",
+      },
+      {
+        title: "inspirations books",
+        url: "https://drive.google.com/drive/folders/1R3hha6OGRB2V3cSi-OEsAhX79rPVkwvg",
+      },
+      {
+        title: "sermons/messages",
+        url: "https://drive.google.com/drive/folders/1jkKYN-DefMlJkHiCsERJ9a3gTt9E3-e9",
+      },
     ],
   },
-  { title: "blog" },
-  { title: "give" },
+  { title: "blog", url: "/blog" },
+  { title: "give", url: "/give" },
 ];
